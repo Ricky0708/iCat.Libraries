093c87612fe41691265ee79a59a268fbab16a5a2
Ricky <ricky0708@lvie.com>
2023-12-26 22:39:39 +0800

    icat.token

---
+++ iCat.Token/src/iCat.Token/Implements/Sequence/TokenGenerator.cs
@@ -0,0 +1,47 @@
+﻿using iCat.Crypto.Interfaces;
+using iCat.Token.Constants;
+using iCat.Token.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCat.Token.Implements.Sequence
+{
+    public class TokenGenerator : ITokenGenerator
+    {
+        private readonly ICryptor _cryptor;
+
+        public string Category => TokenImplementCategory.Sequence;
+
+        public TokenGenerator(ICryptor cryptor)
+        {
+            _cryptor = cryptor ?? throw new ArgumentNullException(nameof(cryptor));
+        }
+
+        public string GenerateToken(List<Claim> claims)
+        {
+            var sb = new StringBuilder();
+            var preClaim = default(Claim);
+            var isContinue = false;
+            foreach (var claim in claims)
+            {
+                if (preClaim?.Type == claim.Type)
+                {
+                    isContinue = true;
+                }
+                if (isContinue && preClaim?.Type != claim.Type)
+                {
+                    sb.Append($"#|");
+                    isContinue = false;
+                }
+                sb.Append($"{claim.Value}|");
+                preClaim = claim;
+            }
+            sb.Remove(sb.Length - 1, 1);
+            return _cryptor.Encrypt(sb.ToString());
+        }
+    }
+}
---
+++ iCat.Token/src/iCat.Token/Implements/Sequence/TokenValidator.cs
@@ -0,0 +1,69 @@
+﻿using iCat.Crypto.Interfaces;
+using iCat.Token.Constants;
+using iCat.Token.Interfaces;
+using iCat.Token.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCat.Token.Implements.Sequence
+{
+    public class TokenValidator<T> : ITokenValidator
+    {
+        private readonly ICryptor _cryptor;
+
+        public string Category => TokenImplementCategory.Sequence;
+
+        public TokenValidator(ICryptor cryptor)
+        {
+            _cryptor = cryptor ?? throw new ArgumentNullException(nameof(cryptor));
+        }
+
+        public ValidationResult Validate(string token)
+        {
+            var type = typeof(T);
+            var tokenValue = _cryptor.Decrypt(token);
+            var claims = new List<Claim>();
+            var result = new ValidationResult
+            {
+                IsValid = false,
+                ErrorMsg = "Decrypt token fail"
+            };
+
+            if (!string.IsNullOrEmpty(tokenValue))
+            {
+                var array = tokenValue.Split('|');
+                var position = 0;
+                foreach (var prop in type.GetProperties())
+                {
+                    if (prop.PropertyType.IsArray ||
+                    prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>) ||
+                    prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                    {
+                        while (position < array.Length && array[position] != "#")
+                        {
+                            claims.Add(new Claim(prop.Name, array[position++]));
+                        }
+                        position++;
+                    }
+                    else
+                    {
+                        claims.Add(new Claim(prop.Name, array[position++]));
+
+                    }
+                }
+
+                var claimIdentity = new ClaimsIdentity(claims);
+                var principal = new ClaimsPrincipal(claimIdentity);
+
+                result.Principal = principal;
+                result.ErrorMsg = "";
+                result.IsValid = true;
+            }
+            return result;
+        }
+    }
+}
