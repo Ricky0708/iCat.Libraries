ee2cc1db144eeba45d1a8528480bcbc2aa4df04d
ricky_huang684 <ricky_huang684@fsoffice.com>
2023-12-27 14:24:26 +0800

    publish bat

---
+++ publish.bat
@@ -0,0 +1,8 @@
+dotnet publish D:\For\iCat.Libraries\iCat.Cache\src\iCat.Cache -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.Crypto\src\iCat.Crypto -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.DB\src\iCat.DB -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.Localization\src\iCat.Localization -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.Logger\src\iCat.Logger -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.RabbitMQ\src\iCat.RabbitMQ -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.Token\src\iCat.Token -f net8.0 -c Release -o released
+dotnet publish D:\For\iCat.Libraries\iCat.Worker\src\iCat.Worker -f net8.0 -c Release -o released
