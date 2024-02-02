49a338fd22d2c7b1e558e4b8701cdcb8db84590f
ricky_huang684 <ricky_huang684@fsoffice.com>
2024-02-02 14:06:31 +0800

    RabbitMQ add new subscriber

--- iCat.RabbitMQ/src/iCat.RabbitMQ/Interfaces/ISubscriber.cs
+++ iCat.RabbitMQ/src/iCat.RabbitMQ/Interfaces/ISubscriber.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 
 namespace iCat.RabbitMQ.Interfaces
 {
+    /// <summary>
+    /// Subscriber
+    /// </summary>
     public interface ISubscriber : IDisposable
     {
-
+        /// <summary>
+        /// Category
+        /// </summary>
         string Category { get; }
 
         /// <summary>
@@ -23,7 +28,7 @@ namespace iCat.RabbitMQ.Interfaces
         IModel Subscribe<T>(string queueName, bool isAutoDeleteQueue, Action<T> processReceived);
 
         /// <summary>
-        /// Subscribe a queue, auto ack
+        /// Subscribe a queue, ack
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="queueName"></param>
@@ -31,5 +36,25 @@ namespace iCat.RabbitMQ.Interfaces
         /// <param name="processReceived"></param>
         /// <returns></returns>
         IModel Subscribe<T>(string queueName, bool isAutoDeleteQueue, Func<T, bool> processReceived);
+
+        /// <summary>
+        /// Subscribe a queue, auto ack
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="queueName"></param>
+        /// <param name="isAutoDeleteQueue"></param>
+        /// <param name="processReceived"></param>
+        /// <returns></returns>
+        IModel SubscribeToString<T>(string queueName, bool isAutoDeleteQueue, Action<string> processReceived);
+
+        /// <summary>
+        /// Subscribe a queue, ack
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="queueName"></param>
+        /// <param name="isAutoDeleteQueue"></param>
+        /// <param name="processReceived"></param>
+        /// <returns></returns>
+        IModel SubscribeToString<T>(string queueName, bool isAutoDeleteQueue, Func<string, bool> processReceived);
     }
 }
