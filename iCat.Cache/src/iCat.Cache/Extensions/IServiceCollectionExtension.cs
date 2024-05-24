46e9a6b51e1965f344721fb967777d033faefc9d
ricky_huang684 <ricky_huang684@fsoffice.com>
2024-05-24 10:13:55 +0800

    cache

---
+++ iCat.Cache/src/iCat.Cache/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,57 @@
+﻿using iCat.Cache.Implements;
+using iCat.Cache.Interfaces;
+using iCat.DB.Client.Factory.Implements;
+using iCat.DB.Client.Factory.Interfaces;
+//using iCat.DB.Client.Factory.Models;
+using iCat.DB.Client.Implements;
+using iCat.DB.Client.Interfaces;
+using iCat.DB.Client.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iCat.DB.Client.Extension.Web
+{
+    /// <summary>
+    /// extension
+    /// </summary>
+    public static class IServiceCollectionExtension
+    {
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddDBClient(this IServiceCollection services, ConfigurationOptions config)
+        {
+            services.TryAddSingleton<ICache, RedisCacheImpl>();
+            services.AddStackExchangeRedisCache(o =>
+            {
+                o.ConnectionMultiplexerFactory = async () =>
+                {
+                    config.AbortOnConnectFail = false;
+                    config.SetDefaultPorts();
+
+                    var connection = await ConnectionMultiplexer.ConnectAsync(config);
+
+                    connection.ConnectionFailed += (_, _) =>
+                        throw new RedisConnectionException(ConnectionFailureType.UnableToConnect, "Connect to Redis fail");
+
+                    return connection;
+                };
+            });
+            return services;
+        }
+    }
+}
